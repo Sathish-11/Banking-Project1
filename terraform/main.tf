965899886c895878af40a980337a4900f8a8cf86
Sathish-11 <just1sathish@gmail.com>
2025-09-20 17:55:07 +0000

    add jenkinsfile

--- terraform/main.tf
+++ terraform/main.tf
@@ -104,6 +104,12 @@ resource "aws_security_group" "finance_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+   ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   ingress {
     from_port   = 9090
     to_port     = 9090
